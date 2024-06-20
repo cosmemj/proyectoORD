fdfb57dff50a0f9ce32324b3bd3a1052c10762e4
Cosme Morillo <cosmemori@gmail.com>
2024-06-20 09:51:41 +0200

    Cambios en la funcionalidad LeerJson

--- CMJ/src/main/java/es/ufv/dis/final2024/CMJ/LectorJson.java
+++ CMJ/src/main/java/es/ufv/dis/final2024/CMJ/LectorJson.java
@@ -13,12 +13,15 @@ public class LectorJson {
     public ArrayList<Nave> leerJson (String fichero) {
         try {
             Gson gson = new Gson();
+
             // Lee el fichero que le pasemos y lo carga en un reader
             Reader reader = Files.newBufferedReader(Paths.get(fichero));
+
             // Convierte el array JSON a un arraylist de users
             ArrayList<Nave> naves = new Gson().fromJson(reader, new TypeToken<ArrayList<Nave>>() {}.getType());
             naves.forEach(System.out::println);// imprime los users
-            reader.close();// close reader
+            reader.close(); // Close reader
+
             return naves;
 
         } catch (Exception ex) {
@@ -26,20 +29,4 @@ public class LectorJson {
             return new ArrayList<>(); // Si no ha leido nada, devuelve un array vacio
         }
     }
-
-    public boolean escribirJson(String fichero, ArrayList<Nave> naves){
-        try {
-            // Lee el fichero que le pasemos y lo abre en modo escritura
-            Writer writer = Files.newBufferedWriter(Paths.get(fichero));
-            // Convierte el arraylist de users a un array JSON, y lo escribe en el fichero
-            writer.write( new Gson().toJson(naves));
-            writer.close();// close reader
-            return true;
-        } catch (Exception ex) {
-            ex.printStackTrace();
-            return false; // Si no ha leido nada, devuelve un array vacio
-        }
-    }
-
-
 }
