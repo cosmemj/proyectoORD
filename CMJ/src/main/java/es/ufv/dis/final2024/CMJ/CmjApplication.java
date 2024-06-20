b26fa65dbb401ca497786f0cbe86c7b8ca8b0a55
Cosme Morillo <cosmemori@gmail.com>
2024-06-20 09:28:54 +0200

    Creación la estructura básica del proyecto

---
+++ CMJ/src/main/java/es/ufv/dis/final2024/CMJ/CmjApplication.java
@@ -0,0 +1,13 @@
+package es.ufv.dis.final2024.CMJ;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CmjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CmjApplication.class, args);
+	}
+
+}
---
+++ CMJ/src/test/java/es/ufv/dis/final2024/CMJ/CmjApplicationTests.java
@@ -0,0 +1,13 @@
+package es.ufv.dis.final2024.CMJ;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CmjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
